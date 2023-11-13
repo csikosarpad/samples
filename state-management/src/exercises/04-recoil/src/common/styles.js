e61ca8d9b8597321c155e01f208b1036fd02f780
Csikós Árpád <arpad.csikos@gmail.com>
2023-11-13 19:25:54 +0100

    react recoil

--- state-management/src/exercises/04-recoil/src/common/styles.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  container: { width: '40%' },
-  item_done: {
-    textDecoration: 'line-through',
-    color: 'gray',
-    fontStyle: 'italic',
-  },
-};
