6c210befedf7e6d215a5cc83ac82f49ea2cb6c86
Csikós Árpád <arpad.csikos@gmail.com>
2024-01-18 10:37:23 +0100

    not interesting

--- typescript/src/01-example.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Allat_1 = require("./Allat");
-var message;
-message = 'Hello TS';
-var consoleDiv = document.querySelector('#console');
-//document.querySelector('h1').innerText = message;
-var allat = new Allat_1.Allat('Pötyi');
-consoleDiv === null || consoleDiv === void 0 ? void 0 : consoleDiv.textContent = allat.nevem();
